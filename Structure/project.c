ba5032556b7353ddc701c84c06a22ca4ff4fea58
Kirill Zakharov <kirill.zakharov00@mail.ru>
2020-04-07 16:53:13 +0300

    Add new functions

--- Structure/project.c
+++ Structure/project.c
@@ -10,7 +10,7 @@ typedef struct list{
 void display(list *start){
 	list *i = start;
 	for ( ; i != NULL; i=i->next){
-		printf("%s\n  ", i->name);
+		printf("%s\n", i->name);
 	}
 }
 
@@ -18,10 +18,8 @@ void display(list *start){
 list* create (list *last){
 	list *i = (list*)malloc(sizeof(list));
 	char name_of_element[10];
-
   printf("Enter the name of the element: ");
   scanf("%s", name_of_element);
-
   i->name = strdup(name_of_element);
 	i->next = NULL;
 	i->prev = NULL;
@@ -54,23 +52,36 @@ list* delete_element(list *last, list *start){
     temp->next = last->next;
     free(last);
 
-		return(temp);
+		return temp;
 }
 
 
-// list* find_number(list *start, int number){
-//     list *p = NULL;
-//     list *i = start;
-//
-//     for ( ; i != NULL ; i=i->next){
-//         if (i->next == number){
-//             p = i;
-//             break;
-//         }
-//     }
-//
-//     return p;
-// }
+list* search_element(list *start, char name){
+    list *tmp = start;
+
+    while(tmp!=NULL){
+			if(tmp->name == name){
+				return tmp;
+			}
+			tmp = tmp->next;
+		}
+
+    return NULL;
+}
+
+list* deleteList(list *start)
+{
+   list *current = start;
+	 list *tmp;
+
+   while (current->next != NULL)
+   {
+       tmp = current->next;
+       free(current);
+       current = tmp;
+   }
+   start = NULL;
+}
 
 int main(){
 
@@ -81,48 +92,50 @@ int main(){
 
 
     while(1){
+				printf("\n");
 				printf("0) Exit the program \n");
         printf("1) Create the first element\n");
-        printf("2) Add item to end\n");
-        printf("3) Remove item from end\n");
-        printf("4) Display\n");
+        printf("2) Add the element to the end\n");
+        printf("3) Remove the element from the end\n");
+        printf("4) Display list\n");
         printf("5) Delete entire list\n");
         printf("6) Search for a given element\n");
         printf("\n");
-
         printf("Enter the command number: \n");
         scanf("%d", &n);
 
-				if(n==0){
+				if(n == 0){
 					exit(1);
 				}
 
-        if(n==1){
+        if(n == 1){
             char name;
 
             i = create(last);
             if (count>=1){
                 last = i;
                 count++;
-                printf("Done! \n");
+								printf("\n");
+                printf(">>The Head element was changed<<\n");
             }
             else {
                 start = i;
                 last = i;
                 count++;
-                printf("Done! \n");
+								printf("\n");
+                printf(">>The Head element was created<<\n");
             }
         }
 
-        if(n==2){
+        if(n == 2){
             i = add_element(last);
             last = i;
-            printf("Done! \n");
+            printf(">>The new element was created<<\n");
         }
 
-        if(n==3){
+        if(n == 3){
             i = delete_element(last, start);
-            printf("Done! \n");
+            printf(">>The last element was deleted<<\n");
         }
 
         if(n == 4){
@@ -133,18 +146,18 @@ int main(){
 						}
         }
 
-        if(n==5){
-					i = start;
-						for (; i != NULL; i=i->next){
+        if(n == 5){
+					i = deleteList(start);
 
-						}
-            printf("Done! \n");
         }
 
-        // if(n==6){
-				// 		i = find_number(3);
-				// 		printf("%d\n", i);
-        // }
+        if(n == 6){
+						char k;
+						printf("Enter the name of the element\n");
+						scanf("%s", &k);
+						i = search_element(start, k);
+						printf("The position of the element is %d\n", i);
+        }
     }
     return 0;
 
